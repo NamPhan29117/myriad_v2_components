e9d117ae5d6e35e294a34db29f364edcc91d9826
NamPhan29117 <phanhainam11797@gmail.com>
2022-03-03 21:41:01 +0700

    test 8

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React from "react";
 import "./App.css";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const DashBoard  = React.lazy(()=>import("./pages/DashBoard"));
-const NotFound = React.lazy(()=>import("./pages/NotFound")); 
-const SettingPage = React.lazy(()=>import("./pages/SettingPage")); ;
-
+const DashBoard = React.lazy(() => import("./pages/DashBoard"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+const SettingPage = React.lazy(() => import("./pages/SettingPage"));
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <React.Suspense fallback={<>Loading...</>}>
-            <DashBoard />
-          </React.Suspense>
-        }>
-          <Route path="/setting" element={
+        <Route
+          path="/"
+          element={
             <React.Suspense fallback={<>Loading...</>}>
-             <SettingPage />
-           </React.Suspense>
-          }/>
-          <Route path="*" element={<NotFound />}/>
+              <DashBoard />
+            </React.Suspense>
+          }
+        >
+          <Route
+            path="/setting"
+            element={
+              <React.Suspense fallback={<>Loading...</>}>
+                <SettingPage />
+              </React.Suspense>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
