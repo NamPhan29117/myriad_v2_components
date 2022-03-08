4900d6f897c82aff7c108b944d55875b03ddc36e
NamPhan29117 <phanhainam11797@gmail.com>
2022-03-08 18:16:54 +0700

    update

--- src/components/TabNavigate/index.jsx
+++ src/components/TabNavigate/index.jsx
@@ -5,15 +5,15 @@ export default function TabNavigate() {
   return (
     <div className="tab-navigate">
       <div className="tab-navigate__item">
-        <img src="/images/big-data.svg"/>
+        <img src="/images/big-data.svg" alt=""/>
         <span>Upload voice</span>
       </div>
       <div className="tab-navigate__item">
-        <img src="/images/email.svg"/>
+        <img src="/images/email.svg" alt=""/>
         <span>Upload image</span>
       </div>
       <div className="tab-navigate__item">
-        <img src="/images/pth-14.svg"/>
+        <img src="/images/pth-14.svg" alt=""/>
         <span>Upload File</span>
       </div>
     </div>
--- src/pages/RecordAudio/index.jsx
+++ src/pages/RecordAudio/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import TabNavigate from '../../components/TabNavigate'
 import './style.scss'
 
 export default function RecordAudio() {
   return (
     <div className="record-audio">
         <h1 className="page-title-s2ix">Create New Record Audio</h1>
+        <TabNavigate />
+        <div className="record-audio-function">
+          <span>Record New Audio</span>
+          <img src="/images/record-icon-big.svg" alt="" />
+        </div>
     </div>
   )
 }
